da9b4f027fd0e46e863e5d1ffe727b62eae39952
Louise Norris <louisenorris73@yahoo.co.uk>
2019-08-20 16:45:46 +0100

    added user form, css on crystal ball and toggle ball functionality, removed has secure password as no longer using password for user

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -5,9 +5,21 @@ class Api::V1::UsersController < ApplicationController
         render json: users
     end
 
-    # def create
-    #     user = User.create(params[:id])
-    # end
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create
+        user = User.find_by(username: params[:username])
+        
+        if user
+        render json: user
+        else
+        user = User.create(username: params[:username])
+        render json: user
+        end
+    end
 
 
 

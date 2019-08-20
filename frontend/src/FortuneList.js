6900ef72405d9220321c4ebad72d972804651175
Louise Norris <louisenorris73@yahoo.co.uk>
2019-08-20 10:51:39 +0100

    added fortuneselect component

--- frontend/src/FortuneList.js
+++ frontend/src/FortuneList.js
@@ -4,7 +4,7 @@ import FortuneCard from './FortuneCard';
 class FortuneList extends Component {
     render() {
         return (
-            <div>
+            <div className='master-detail-element sidebar'>
                 {
                     this.props.fortunes.map(fortune => < FortuneCard fortune={fortune}/>)
                 }

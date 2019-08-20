fa1f9e0c8c762c65899b78a50ca1c888e98102de
Louise Norris <louisenorris73@yahoo.co.uk>
2019-08-20 12:58:10 +0100

    made slashes in navbar blue and changed background navbar colour slightly

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return (
             <div className="nav-bar">
-                <h1 className="nav-item">// Flatiron Fortune Teller</h1>
+                <h1 className="nav-item"><span className="textColor">//</span> Flatiron Fortune Teller</h1>
             </div>
         );
     }

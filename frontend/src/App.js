7dcb8cb8b2e7d2d9d55324529ddf5626a9d747a6
Louise Norris <louisenorris73@yahoo.co.uk>
2019-08-21 11:21:12 +0100

    added select random fortune and show it on click in FortuneSelect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 const fortunesURL = "http://localhost:3000/api/v1/user_fortunes"
 const usersURL = "http://localhost:3000/api/v1/users"
+// const fortunesToRandomize = this.state.selectedNewFortunes
+// const newFortunes = "http://localhost:3000/api/v1/fortunes"
 
 
 class App extends Component {
@@ -12,14 +14,25 @@ class App extends Component {
   state = {
     fortunes: [],
     selectedUser: null,
-    //newFortune
+    newFortunes: [],
+    selectedFortune: []
   }
 
   componentDidMount() {
     fetch(fortunesURL)
     .then(resp => resp.json())
     .then(fortunes => this.setState({fortunes}))
+    
+    fetch("http://localhost:3000/api/v1/fortunes")
+    .then(resp => resp.json())
+    .then(newFortunes => this.setState({newFortunes}))
   }
+
+  getRandomFortune = () => {
+    let fortunesToRandomize = this.state.newFortunes
+    let selectedAtRandom = fortunesToRandomize[Math.floor(Math.random() * fortunesToRandomize.length)];
+    this.setState({selectedFortune: selectedAtRandom})
+  } 
 
   handleChange = (event) => {
     this.setState({selectedUser: event.target.value})
@@ -39,11 +52,20 @@ class App extends Component {
     
   };
 
+
   render() {
     return (
       <div className="app">
-             <Navbar user={this.state.selectedUser} handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
-             <FortuneContainer fortunes={this.state.fortunes}/>
+             <Navbar 
+                user={this.state.selectedUser} 
+                handleSubmit={this.handleSubmit} 
+                handleChange={this.handleChange}
+              />
+             <FortuneContainer 
+                fortunes={this.state.fortunes}
+                getRandomFortune={this.getRandomFortune}
+                selectedFortune={this.state.selectedFortune}
+              />
       </div>
     );
   }

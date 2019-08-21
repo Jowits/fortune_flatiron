f9e12e66169fa8df17854b6a278c0a1ec9921138
Jowita <jowita.lapies@gmail.com>
2019-08-21 10:32:04 +0100

    change the look

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
 
 class Navbar extends Component {
     render() {
         return (
             <div className="nav-bar"> 
-                <h1 className="nav-item"><span className="textColor">//</span> Flatiron Fortune Teller</h1>
+                <h1 className="nav-item"><span>//</span> Flatiron Fortune Teller</h1>
                 <form onSubmit={this.props.handleSubmit} onChange={(event) => this.props.handleChange(event)}>
                 <label className="textColor">
                     {"Name: "} 
